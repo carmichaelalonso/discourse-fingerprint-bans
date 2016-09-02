6182e9d76fd875dca0cf0661fc2959b57a865dd1
Cameron Carmichael Alonso <cameron@carmichaelalonso.co.uk>
2016-09-02 21:46:48 +0200

    Added project URL

--- plugin.rb
+++ plugin.rb
@@ -2,3 +2,4 @@
 # about: Plugin for staff users to ban specific browser fingerprints
 # version: 1.0.0
 # authors: Cameron Carmichael Alonso
+# url: https://github.com/carmichaelalonso/discourse-fingerprint-bans
